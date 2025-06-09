1f459fda5ad5232a5a2ee008317bafe5cecb4bff
ysk1o <yusuke-fujii@politive.co.jp>
2025-06-09 10:58:17 +0900

    v1.2.1対応

--- Formula/kimigayo.rb
+++ Formula/kimigayo.rb
@@ -1,8 +1,8 @@
 class Kimigayo < Formula
   desc "Play Japan’s national anthem in your terminal"
   homepage "https://github.com/politive/kimigayo"
-  url "https://github.com/politive/kimigayo/archive/refs/tags/v1.2.0.tar.gz"
-  sha256 "c46b41e9e6367e0579a27d452b3c38d77c1b18720d312ed668273ca435062601"
+  url "https://github.com/politive/kimigayo/archive/refs/tags/v1.2.1.tar.gz"
+  sha256 "6a71159351c37feb34a951e93def8b806d40beadbdfd51aab57fb03e6015a052"
   license "MIT"
 
   def install
